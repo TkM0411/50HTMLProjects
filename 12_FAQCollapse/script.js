9adb3f25cdc557bf844dd028c2c55d778e7cb975
Tushar Kanti Mishra <tushar.mishra1165@gmail.com>
2025-11-08 21:15:26 +0530

    Day12: Added code implementation for FAQ Toggle.

---
+++ 12_FAQCollapse/script.js
@@ -0,0 +1,6 @@
+const toggleButtons = document.querySelectorAll('.faq-toggle');
+toggleButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.classList.toggle('active');
+    });
+});
